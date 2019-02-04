b07e468a96153ba87be74801759a47824fddb162
Mrudul Dubey <mrudulmad29@gmail.com>
2019-02-04 16:38:44 -0500

    Icons changed

---
+++ public/Scripts/app.js
@@ -0,0 +1,10 @@
+//custom javascript
+//IIFE- Immediately invoked Fucntion Express
+
+(function(){
+    function Start(){
+        console.log(`%c App Satrted...`,"font-size: 20px; color: blue; font-weight: bold;");
+    }
+
+    window.addEventListener("load",Start);
+})();
